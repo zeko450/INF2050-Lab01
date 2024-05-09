9e7f544bfd964ff26948e3ca41589ebe349b1a4e
Zeko450 <moh-kach@hotmail.com>
2024-05-09 19:17:28 -0400

    3eme commit

--- src/lab01/main.java
+++ src/lab01/main.java
@@ -16,7 +16,6 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-  
+        System.out.println("Bienvenue, comment ca va ");
     }
-    
 }
